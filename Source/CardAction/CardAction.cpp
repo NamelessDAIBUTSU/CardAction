7689eaf76a8f22f55dfa789cb10a7674b3219e91
ashbuns <ashbuns828@gmail.com>
2025-09-27 03:48:25 +0900

    Initial commit: UE5 project with .gitignore and LFS

---
+++ Source/CardAction/CardAction.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CardAction.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CardAction, "CardAction" );
+
+DEFINE_LOG_CATEGORY(LogCardAction)
+ 
