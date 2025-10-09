0623b512ffd6c71af5a8ffce89f6258ba08e5ae5
ILCA-2382\kanze_motohide <kanze_motohide@ilca.co.jp>
2025-10-09 11:16:14 +0900

    CardSelectフェーズの追加

--- Source/CardAction/Private/System/Phase/BattlePhase_Action.cpp
+++ Source/CardAction/Private/System/Phase/BattlePhase_Action.cpp
@@ -2,10 +2,20 @@
 
 
 #include "System/Phase/BattlePhase_Action.h"
+#include <Kismet/GameplayStatics.h>
+#include <System/MyGameMode.h>
 
 // フェーズ中
-void UBattlePhase_Action::OnTick(float DeltaSec) 
+void UBattlePhase_Action::OnTick(float DeltaSec)
 {
 	// 敵が全滅していたらフェーズを切り替えフラグを立てる
+	AGameModeBase* GameMode = UGameplayStatics::GetGameMode(this);
+	if (AMyGameMode* MyGameMode = Cast<AMyGameMode>(GameMode))
+	{
+		AGridManager* GridManager = MyGameMode->GridManager;
+		if (GridManager == nullptr)
+			return;
 
+		bRequestChangePhase = (GridManager->IsExistEnemyOnGrid() == false);
+	}
 }
---
+++ Source/CardAction/Private/System/Phase/BattlePhase_CardSelect.cpp
@@ -0,0 +1,5 @@
+#include "System/Phase/BattlePhase_CardSelect.h"
+
+void UBattlePhase_CardSelect::OnTick(float DeltaSec)
+{
+}
